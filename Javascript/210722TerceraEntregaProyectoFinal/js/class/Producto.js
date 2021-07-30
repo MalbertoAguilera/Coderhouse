dca43671e087a79cb78a80b8840bd21dcf1cc1a2
Matias Aguilera <malbertoaguilera@outlook.es>
2021-07-29 23:18:30 -0300

    Tercera Entrega Proyecto Final

---
+++ Javascript/210722TerceraEntregaProyectoFinal/js/class/Producto.js
@@ -0,0 +1,11 @@
+class Producto {
+
+      constructor(id = "", nombre = "", tipo = "", precio = 0, cantidad = "", enlace) {
+            this.id = id;
+            this.nombre = nombre;
+            this.tipo = tipo;
+            this.precio = precio;
+            this.cantidad = cantidad;
+            this.thumbnailUrl = enlace;
+      };
+};
---
+++ Javascript/210722TerceraEntregaProyectoFinal/js/variables.js
@@ -0,0 +1,20 @@
+//punteros a document
+const gridProductos = document.querySelector('.gridProductos');
+const itemCarrito = document.querySelector('#itemCarrito');
+const footerTabla = document.querySelector('#footerTabla');
+const iconoCarrito = document.querySelector('#contenedorIcono');
+const contenedorNotificaciones = document.querySelector('#contenedorNotificaciones');
+//punteros a template
+const templateCard = document.querySelector('#templateCard').content;
+const templateCarrito = document.querySelector('#templateCarrito').content;
+const templateFooter = document.querySelector('#templateFooter').content;
+const fragment = document.createDocumentFragment();
+//ruta imagenes
+const pathImagen = "./assets/img/"
+
+const metodo = new Metodos();
+let carrito = new CarritoDeCompra();
+let productos;
+let datos;
+
+const contenedorPanelCarrito = document.querySelector('#contenedorPanelCarrito');
