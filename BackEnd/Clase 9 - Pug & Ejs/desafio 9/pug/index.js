edd786542ae382b4b6d8a03015b4cdd109121caa
Matias Aguilera <malbertoaguilera@outlook.es>
2021-11-06 20:27:38 -0300

    pug

---
+++ BackEnd/Clase 9 - Pug & Ejs/desafio 9/pug/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = require("./routes/index");
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+app.set("view engine","pug")
+app.set("views", "./views")
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+
+//evita enviar staticos por defecto
+app.use("/static",express.static(__dirname + "/public"));
+
+app.get('/', (req , res)=>{
+
+    res.render("main") 
+ 
+ })
+
+app.use("/productos", router);
+
+const server = app.listen(PORT, () => {
+      console.log(`El servidor se encuentra escuchando por el puerto ${server.address().port}`)
+});
+
+server.on("error", error => console.log(`Error en servidor ${error}`));
